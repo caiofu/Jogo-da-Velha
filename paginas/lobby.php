ed3667d1013f27b328df7d3f3068ae99150e1817
Caio Furegati <caiofu@hotmail.com>
2022-11-08 17:26:23 -0300

    Title lobby

--- paginas/lobby.php
+++ paginas/lobby.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>partidas</title>
+    <title>Lobby</title>
     <script src="../js/jogo.js"></script>
     <link rel="stylesheet" href="../css/index.css">
     
